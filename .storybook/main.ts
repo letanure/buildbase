1fc195e8a1f53c46f8edae7b40b903b5bc29fad5
letanure <letanure@gmail.com>
2025-06-09 15:21:48 +0200

    feat: add storybook-next-intl to Storybook configuration for improved localization support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,8 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
+    'storybook-next-intl',
   ],
   "framework": {
     "name": "@storybook/nextjs-vite",
